9c6cefcd24e3921006394a44300b8c9cb4a32055
Jeremiah Michaels <jam.jeremiah88@gmail.com>
2025-01-07 16:49:23 -0500

    Added animations, fixed player movement, and added UI prototype

--- new-game-project/Scripts/playerScript.gd
+++ new-game-project/Scripts/playerScript.gd
@@ -3,37 +3,52 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
-var prevVelocity: Vector2 = Vector2.ZERO #Tried to implement air friction
 var time_last_pressed = 0 #gonna use for coyote time for jump
 var time_now_pressed = 0 #gonna use for coyote time for jump
 
-
-
+var can_increase_height = true
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		$PlayerSprite.animation = "InAir"
+	else:
+		can_increase_height = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		$PlayerSprite.play("Jump")
 		velocity.y = JUMP_VELOCITY
-
-	if Input.is_action_just_released("ui_accept"):
-		velocity.y *= 0.4 #The longer you hold W for jump, the higher you go
-
-	if not is_on_floor():
-		velocity.x = lerp(prevVelocity.x, velocity.x, 1) 
+		$JumpHeightTimer.start()
+		
+	if Input.is_action_just_released("jump") and can_increase_height:
+		velocity.y *= 0.4 #The longer you hold for jump, the higher you go
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if velocity.x != 0 and is_on_floor():
+		$PlayerSprite.animation = "Walk"
+	elif is_on_floor():
+		$PlayerSprite.animation = "Idle"
 	
+	if velocity.x > 0:
+		$PlayerSprite.flip_h = false
+		$RayCast2D.target_position = Vector2i(256, 0)
+	elif velocity.x < 0:
+		$PlayerSprite.flip_h = true
+		$RayCast2D.target_position = Vector2i(-256, 0)
 	
 	move_and_slide()
 	
+	print(to_local($RayCast2D.get_collision_point()))
+
+func _on_jump_height_timer_timeout() -> void:
+	can_increase_height = false
